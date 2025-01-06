7b2f5c2f999760f714102bc2ed1c96f196754729
Nageen77 <nageenshaik9999@gmail.com>
2025-01-06 14:57:48 +0530

    cra & tailwind css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="font-3xl font-bold text-green-800">
+      <header className="App-header">
+        <p>
+         Hello React !!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
